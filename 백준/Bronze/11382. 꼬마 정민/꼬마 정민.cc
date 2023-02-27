3923744420057bb719528488cb637ca778049eb5
JiyeongJung <124158811+JungJiYeoung@users.noreply.github.com>
2023-02-28 01:26:37 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	long long A, B, C;
+
+	cin >> A >> B >> C;
+	cout << A + B + C;
+
+
+
+    return 0;
+}
